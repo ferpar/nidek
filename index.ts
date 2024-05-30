3c03fa54bc87cf46939110ebe3f11631f9caf8cd
Fer <ferp@protonmail.ch>
2024-05-30 17:36:30 +0200

    login success

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
-import { chromium } from 'playwright';
+import { chromium } from "playwright";
+const username = String(Bun.env.USR);
+const password = String(Bun.env.PSS);
 
 (async () => {
-    console.log("Starting browser...")
-    const browser = await chromium.launch();
+  console.log("Starting browser...");
+  const browser = await chromium.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto("https://www.linkedin.com/");
+  await page.getByRole("link", { name: "Sign in", exact: true }).click();
+  await page.getByLabel("Email or Phone").fill(username);
+  await page.getByLabel("Password").click();
+  await page.getByLabel("Password").fill(password);
+  await page.getByRole("button", { name: "Sign in", exact: true }).click();
+  const label = page.getByLabel("Side bar").getByText("Full Stack Developer ");
+  console.log(label);
 
-    console.log("Creating context...")
-
-    await browser.close()
-    console.log("Browser closed.")
-})()
+  // await browser.close()
+  console.log("Browser closed.");
+})();
