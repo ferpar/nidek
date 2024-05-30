51d4c7d40abc434854957bd1268c95f7ba36a311
Fer <ferp@protonmail.ch>
2024-05-30 15:11:23 +0200

    base test server

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+console.log("Starting server via Bun!");
+
+const server = Bun.serve({
+    port: 3002,
+    fetch(req) {
+        return new Response("Bun!")
+    },
+})
+
+console.log(`Listening on http://localhost:${server.port} ...`)
