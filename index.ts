63c1cb394e1b2a7e7fe7fa45860f1618df8f72bf
Fer <ferp@protonmail.ch>
2024-06-03 19:17:16 +0200

    improve robustness

--- index.ts
+++ index.ts
@@ -34,36 +34,52 @@ import { login } from "./utils/userUtils";
   // navigate to jobs page
   await page.getByRole("link", { name: "Jobs", exact: true }).click();
   console.log("clicked on jobs");
+
+  // perform job search
   const searchField = await page.getByRole("combobox", {
     name: "Search by title, skill, or",
   });
-  searchField.click();
-  searchField.fill("react");
+  await searchField.click();
+  await searchField.fill("react");
   const locationField = await page.getByRole("combobox", {
     name: "City, state, or zip code",
   });
-  locationField.fill("Europe");
-  locationField.press("Enter");
+  await locationField.fill("european union");
+  await searchField.click();
+  await searchField.press("Enter");
   console.log("filled in search");
 
   // apply remote filter
-  const remoteDropdown = page.getByLabel("Remote filter.")
+  let searchFiltersBar = page.locator(".search-filters-bar")
+  const remoteDropdown = searchFiltersBar.getByLabel("Remote filter.")
   await remoteDropdown.click();
   const remoteMenu = page.getByRole('group', { name: 'Filter results by: Remote' });
   const remoteCheckbox = remoteMenu.getByText("Remote", {exact: true} )
   await remoteCheckbox.click()
+  await page.waitForTimeout(500);
   await remoteMenu.getByRole('button', { name: 'Apply current filter to show' }).click();
 
   // apply date posted filter
-  const datePostedDropdown = page.getByLabel("Date posted filter.")
+  searchFiltersBar = page.locator(".search-filters-bar")
+  const datePostedDropdown = searchFiltersBar.getByLabel("Date posted filter.")
   datePostedDropdown.click();
   const datePostedMenu = page.getByRole('group', { name: 'Filter results by: Date posted' });
   const past24HoursRadioBtn = datePostedMenu.getByText("Past 24 hours", {exact: true} )
   await past24HoursRadioBtn.click();
+  await page.waitForTimeout(200);
   await datePostedMenu.getByRole('button', { name: 'Apply current filter to show' }).click();
+  
+  // get job description
+  let jobDescription = page.locator('.jobs-description__container')
+  console.log("Job description: ", await jobDescription.textContent());
 
   // go to page 2
   await page.getByLabel('Page 2').click();
+  await page.waitForTimeout(200);
+
+  // get job description
+  jobDescription = page.locator('.jobs-description__container')
+  console.log("Job description: ", await jobDescription.textContent());
 
   // await browser.close();
   // console.log("Browser closed.");
